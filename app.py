b7187ed9d1d02aa004e69e69a688f559bce59199
Vinayak Thapa <88424003+vthapa95@users.noreply.github.com>
2025-11-06 23:07:59 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello from Docker!")
+from flask import Flask
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route("/")
+def home():
+    return "Hello World from Flask running inside Docker!"
+
+# Run the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
